eadb084391547ffe97bfa5a0f40cb20f567a5c36
Harshit Kumar Singh <156910994+HarshitBuilds@users.noreply.github.com>
2025-03-23 14:42:31 +0530

    Deleted window.h
    Not required anymore

--- window.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WINDOW_H
-#define _WINDOW_H
-#include "header.h"
-class Window 
-{
-public:
-	vector<int> indices; //vector to store the indices of all the ants at a given timestep
-};
-#endif
