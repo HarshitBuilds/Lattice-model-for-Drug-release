9941754d32cb4cc22f001dc73c12d0ed4c20945e
HarshitBuilds <harshit.krsingh2004@gmail.com>
2025-06-03 08:06:10 +0530

    Minor fix

--- process.sh
+++ process.sh
@@ -21,7 +21,7 @@ is_top=0 #initialising ants 0 for bottom layer (default), 1 for top layer
 tau_inc=1000 #add parameter for tau interval spacing.  
 for ((r=1;r<=mcruns;r++)); 
 do
-    ../ANT --WALLF1 "$w1" --WALLF2 "$w2" -p "$p" -a "$a" -r "$r" -s "$sweeps" -S 1000000 -n "$n" -x "$x" --PJUMP "$pj" --is_top "$is_top" --tau_inc "$tau_inc"
+    ../ANT --WALLF1 "$w1" --WALLF2 "$w2" -PWALL "$p" -a "$a" -r "$r" -s "$sweeps" -S 1000000 -n "$n" -x "$x" --PJUMP "$pj" --is_top "$is_top" --tau_inc "$tau_inc"
 done
 python3 ../process.py "$mcruns" "$a" "$w1" "$w2" "$x" "$sweeps" "$is_top" "$tau_inc" "$n" #processing part of the code 
 find . -maxdepth 1 -name "*.dat" -print0 | xargs -0 rm #to remove the .dat files produced 
